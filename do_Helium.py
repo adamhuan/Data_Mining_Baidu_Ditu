1a26f4ffb49e6e09c72d328b0cd358a4178aac6f
unknown <dkseven_eli@163.com>
2020-07-13 00:29:42 +0800

    修复多页检索结果的时候，爬取提前退出的问题

--- do_Helium.py
+++ do_Helium.py
@@ -462,7 +462,9 @@ class class_baidu_ditu:
 
                 # 处理
                 if obj_current_navg_item.text != "下一页>" \
-                    and obj_current_navg_item.text != "<上一页":
+                    and obj_current_navg_item.text != "<上一页" \
+                    and obj_current_navg_item.text != "首页" \
+                    and obj_current_navg_item.text != "尾页":
                     obj_current_navg_list.append(int(obj_current_navg_item.text))
             
             print("当前【导航条】包含页码：" + str(obj_current_navg_list))
